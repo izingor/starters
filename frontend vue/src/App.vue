1cd1d31b394303480465fd14466345b5172ce94e
igz <izingor@gmail.com>
2022-04-07 14:58:59 +0300

    vue starter

---
+++ frontend vue/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+	<section>
+		<header />
+
+		<router-view></router-view>
+	</section>
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
---
+++ frontend vue/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <main>
+      <h1>Hello mister tasker</h1>
+      <p></p>
+  </main>
+</template>
+<script>
+
+</script>
