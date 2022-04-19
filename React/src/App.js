490a47a137971a395411473060cd69b2f1eb0955
Igor Zis <izingor@gmail.com>
2022-04-19 17:02:40 +0300

    Add files via upload

---
+++ React/src/App.js
@@ -0,0 +1,14 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import './assets/scss/global.scss';
+
+function App() {
+  return (
+    <section class="main-app">
+      <Header />
+      <Footer />
+    </section>
+  );
+}
+
+export default App;
---
+++ React/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import { Component } from 'react';
+
+export class Header extends Component {
+	render() {
+		return <div className="main-header">I am the header</div>;
+	}
+}
